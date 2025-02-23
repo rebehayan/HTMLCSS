9302d65443398deea490c7c00ac08a1c9b99ed54
rebehayan <rebehayan@gmail.com>
2025-02-23 14:07:08 +0900

    수업자료

--- guide/global.js
+++ guide/global.js
@@ -1,3 +1,15 @@
+/* -------------------------------------------------------------------------- */
+/*                                     설정                                   */
+/* -------------------------------------------------------------------------- */
+const config = {
+  active: "--active",
+  storageName: "activeNav",
+};
+const { active, storageName } = config;
+
+/* -------------------------------------------------------------------------- */
+/*                                     GNB                                    */
+/* -------------------------------------------------------------------------- */
 const GNB = () => {
   const header = document.querySelector(".guide-header");
   if (!header) return;
@@ -7,58 +19,93 @@ const GNB = () => {
   const bindEvent = (e) => {
     removeClass();
     addClass(e);
-    insertHeading(e);
-    iframe();
+    setState(e);
+    insertHeading();
   };
 
   // 클래스추가
   const addClass = (event) => {
-    event.target.classList.add("--active");
+    event.target.classList.add(active);
   };
 
   // 클래스제거
   const removeClass = () => {
     links.forEach((link) => {
-      link.classList.remove("--active");
+      link.classList.remove(active);
     });
   };
 
-  // 제목삽입
-  const insertHeading = (e) => {
-    const text = e.target.textContent;
-    const href = e.target.getAttribute("href");
-    const main = document.querySelector(".guide-main");
-    const contents = main.querySelector("#contents");
-    const heading = main.querySelector("#heading");
-
-    document.title = `${text} - Rebehayan`; // <title> 변경
-
-    if (heading) {
-      heading.querySelector("span").textContent = text;
-      heading.querySelector("a").setAttribute("href", href);
-    } else {
-      contents.insertAdjacentHTML("beforebegin", `<h2 id="heading"><span>${text}</span><a href="${href}" target="_blank">새창 보기</a></h2>`);
-    }
+  // 상태저장
+  const setState = (e) => {
+    sessionStorage.setItem(storageName, e.target.getAttribute("href"));
+    sessionStorage.setItem("activeText", e.target.textContent);
   };
 
+  // 이벤트실행
   links.forEach((link) => {
     link.removeEventListener("click", bindEvent); // 메모리누수 방지
     link.addEventListener("click", bindEvent);
   });
 };
 
-const iframe = () => {
+/* -------------------------------------------------------------------------- */
+/*                                  새로고침 상태관리                            */
+/* -------------------------------------------------------------------------- */
+const getState = () => {
   const frame = document.querySelector("iframe");
   if (!frame) return;
 
-  frame.onload = null;
-  frame.onload = () => {
-    try {
-      const doc = frame.contentDocument || frame.contentWindow.document;
-    } catch (error) {
-      console.log(error);
+  // iframe
+  const activeNav = sessionStorage.getItem(storageName);
+  frame.setAttribute("src", activeNav);
+
+  // Nav Active
+  const gnbs = document.querySelectorAll('[name="gnb"]');
+  const links = document.querySelectorAll('[name="gnb"] a');
+  links.forEach((link) => {
+    if (link.getAttribute("href") === activeNav) {
+      link.classList.add(active);
     }
-  };
+  });
+  gnbs.forEach((gnb) => {
+    if (gnb.querySelector(`.${active}`)) {
+      gnb.setAttribute("open", "true");
+    }
+  });
 };
 
+function throttle(callback, delay) {
+  let lastTime = 0;
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastTime >= delay) {
+      lastTime = now;
+      callback.apply(this, args);
+    }
+  };
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    제목삽입                                 */
+/* -------------------------------------------------------------------------- */
+const insertHeading = () => {
+  const text = sessionStorage.getItem("activeText");
+  const href = sessionStorage.getItem("activeNav");
+  const main = document.querySelector(".guide-main");
+  const contents = main.querySelector("#contents");
+  const heading = main.querySelector("#heading");
+
+  document.title = `${text} - Rebehayan`; // <title> 변경
+
+  if (heading) {
+    heading.querySelector("span").textContent = text;
+    heading.querySelector("a").setAttribute("href", href);
+  } else {
+    contents.insertAdjacentHTML("beforebegin", `<h2 id="heading"><span>${text}</span><a href="${href}" target="_blank">새창 보기</a></h2>`);
+  }
+};
+
+/* ---------------------------------- 함수실행 ---------------------------------- */
 GNB();
+insertHeading();
+getState();
